9104a17927d01f454d7330ea4ba9cc4e6c52a8fe
Chris Sutton <chris_sutton@hey.com>
2023-12-14 19:57:31 -0600

    adds some styling

--- pages/assessments.tsx
+++ pages/assessments.tsx
@@ -1,3 +1,4 @@
+import AssessmentCard from "@/components/AssessmentCard";
 import { useState, useEffect } from "react";
 
 const AssessmentsPage = () => {
@@ -27,54 +28,10 @@ const AssessmentsPage = () => {
 
   return (
     <div>
-      <h1>Assessments</h1>
-      <table className="w-screen">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Company</th>
-            <th>URL</th>
-            <th>Job Title</th>
-            <th>Grade</th>
-            <th>Rater</th>
-            <th>Tech</th>
-            <th>Skills</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {assessments &&
-            assessments.map((assessment) => (
-              <tr key={assessment.id}>
-                <td>{assessment.id}</td>
-                <td>{assessment.companyName}</td>
-                <td>{assessment.url}</td>
-                <td>{assessment.jobTitle}</td>
-                <td>{assessment.grade}</td>
-                <td>{assessment.rater}</td>
-                <td>
-                  Matching:{" "}
-                  {assessment.matchingTech && assessment.matchingTech.join(", ")}
-                  <br />
-                  Missing:{" "}
-                  {assessment.missingTech && assessment.missingTech}
-                </td>
-                <td>
-                  Matching:{" "}
-                  {assessment.matchingSkills && assessment.matchingSkills}
-                  <br />
-                  Missing:{" "}
-                  {assessment.missingSkills && assessment.missingSkills}
-                </td>
-                <td>
-                  <button onClick={() => handleDelete(assessment.id)}>
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
+      <h1 className="text-4xl">Assessments</h1>
+      {assessments
+        ? assessments.map((assessment) => AssessmentCard({ assessment }))
+        : null}
     </div>
   );
 };
