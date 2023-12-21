bdc8a70a4888d784ac803f00b6eb9f5ee46f5933
Chris Sutton <chris_sutton@hey.com>
2023-12-20 19:33:10 -0600

    homepage stub

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,16 @@ const HomePage = () => {
   return (
     <>
       <main className="flex-1">
-        <NewPostingForm />
+        <div className="flex flex-col items-center justify-center h-full">
+          <div className="flex flex-col items-center justify-center space-y-4">
+            <h1 className="text-5xl font-bold text-slate-200">
+              Welcome to Job Board
+            </h1>
+            <p className="text-xl text-slate-200">
+              Create a new job posting to get started
+            </p>
+          </div>
+        </div>
       </main>
 
       <aside className="sticky top-8 hidden w-96 shrink-0 xl:block">
